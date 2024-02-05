42212444f34182c0f511ad3d6cf8ab3d415f91aa
gayoung <gyy94@naver.com>
2024-02-06 00:13:07 +0900

    day01

--- src/day01/Ex03.java
+++ src/day01/Ex03.java
@@ -6,10 +6,16 @@ public class Ex03 {
 		System.out.println(1.123+3.456);
 		System.out.println("안녕"+"하세요");
 		System.out.println("100"+200);
+		System.out.println("100"+200);
+		System.out.println("100"+200);
 		System.out.println("안녕"+100+100);
 		System.out.println("안녕"+(100+100));
 		System.out.println("안녕"+(100+100));
 		
+		System.out.println("=====================");
+		System.out.println("=====================");
+		System.out.println("=====================");
+		System.out.println("=====================");
 		System.out.println("=====================");
 		System.out.println("=====================");
 		System.out.println("=====================");
---
+++ src/day01/Ex05.java
@@ -0,0 +1,9 @@
+package day01;
+
+public class Ex05 {
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
+}
