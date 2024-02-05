84314360f681f9589dfc66e513535d549d633480
yungayeong <gyy94@naver.com>
2024-02-05 17:04:46 +0900

    수정1

--- src/day01/Ex02.java
+++ src/day01/Ex02.java
@@ -12,9 +12,9 @@ public class Ex02 {
 		System.out.println("=====================================");
 		System.out.println("이름\t나이\t전화번호\t       회비");
 		System.out.println("=====================================");
-		System.out.println("홍길동\t 15\t3672-1234\t\\20000");
-		System.out.println("고길동\t 15\t2238-1234\t\\30000");
-		System.out.println("김말이\t 15\t1234-1234\t\\50000");
+		System.out.println("홍길동\t\"15\"\t3672-1234\t\\20000");
+		System.out.println("고길동\t\"15\"\t2238-1234\t\\30000");
+		System.out.println("김말이\t\"15\"\t1234-1234\t\\50000");
 		System.out.println("-------------------------------------");
 		System.out.println("총합계\t\t                \\10000 ");
 		System.out.println("-------------------------------------");
